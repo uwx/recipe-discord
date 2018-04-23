5fd7bccb6fe2af1f20dabd60fa270cbc0de02a2f
uwx <cadrekucra@gmx.com>
2018-04-22 23:08:25 -0300

    discordjs-RPC-mod

---
+++ scriptycord/discordjs-RPC-mod/example/main.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-console */
+
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+const DiscordRPC = require('../');
+
+// don't change the client id if you want this example to work
+const ClientId = '180984871685062656';
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 340,
+    height: 380,
+    resizable: false,
+    titleBarStyle: 'hidden',
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true,
+  }));
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+app.on('activate', () => {
+  if (mainWindow === null)
+    createWindow();
+});
+
+// only needed for discord allowing spectate, join, ask to join
+DiscordRPC.register(ClientId);
+
+const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+const startTimestamp = new Date();
+
+async function setActivity() {
+  if (!rpc || !mainWindow)
+    return;
+
+  const boops = await mainWindow.webContents.executeJavaScript('window.boops');
+
+  rpc.setActivity({
+    details: `booped ${boops} times`,
+    state: 'in slither party',
+    startTimestamp,
+    largeImageKey: 'snek_large',
+    largeImageText: 'tea is delicious',
+    smallImageKey: 'snek_small',
+    smallImageText: 'i am my own pillows',
+    instance: false,
+  });
+}
+
+rpc.on('ready', () => {
+  setActivity();
+
+  // activity can only be set every 15 seconds
+  setInterval(() => {
+    setActivity();
+  }, 15e3);
+});
+
+rpc.login(ClientId).catch(console.error);
---
+++ scriptycord/discordjs-RPC-mod/test/rp.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/* eslint-disable no-console */
+
+try {
+  require('wtfnode').init();
+} catch (err) {} // eslint-disable-line no-empty
+
+const { Client } = require('../');
+
+const clientID = '180984871685062656';
+
+const client = new Client({ transport: 'ipc' });
+
+client.on('ready', () => {
+  client.subscribe('ACTIVITY_JOIN', ({ secret }) => {
+    console.log('should join game with secret:', secret);
+  });
+
+  client.subscribe('ACTIVITY_SPECTATE', ({ secret }) => {
+    console.log('should spectate game with secret:', secret);
+  });
+
+  client.subscribe('ACTIVITY_JOIN_REQUEST', (user) => {
+    console.log('user wants to join:', user);
+  });
+
+  client.setActivity({
+    state: 'slithering',
+    details: '🐍',
+    startTimestamp: new Date(),
+    largeImageKey: 'snek_large',
+    smallImageKey: 'snek_small',
+    partyId: 'snek_party',
+    partySize: 1,
+    partyMax: 1,
+    matchSecret: 'slithers',
+    joinSecret: 'boop',
+    spectateSecret: 'sniff',
+    instance: true,
+  }).then(console.log);
+});
+
+client.login(clientID).catch(console.error);
