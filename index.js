6eea9c2d57b0d03afe0a32576c8560ff968e8684
uwx <cadrekucra@gmx.com>
2018-04-23 02:29:53 -0300

    add maximize hook

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ module.exports = Franz => class Discord extends Franz {
     return 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.204 Chrome/56.0.2924.87 Discord Canary/1.6.15 Safari/537.36';
   }
 };
+
+const win = require('electron').remote.getCurrentWindow();
+win.on('unmaximize', () => {
+  setTimeout(() => win.maximize(), 0);
+})
