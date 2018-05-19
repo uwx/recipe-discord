b2a13325beb27416697a05bc5e134fb4bc14a2a6
uwx <cadrekucra@gmx.com>
2018-05-19 01:39:56 -0300

    handle storage missing or invalid (thanks kaffei!)

--- scriptycord/storage.js
+++ scriptycord/storage.js
@@ -6,7 +6,19 @@ const fs = require('fsxt');
 const { userDataRoot } = require('./paths.js');
 
 exports.load = async () => {
-  const storageData = JSON.parse(await fs.readFile(userDataRoot + '/storage.json', 'utf8'));
+  let storageData;
+  try {
+    const storageFile = await fs.readFile(userDataRoot + '/storage.json', 'utf8');
+    if (!storageFile) {
+      console.log('[scriptycord/storage] file is empty!');
+      storageData = {};
+    } else {
+      storageData = JSON.parse(storageFile);
+    }
+  } catch (e) {
+    console.error('[scriptycord/storage] during load: ', e);
+    storageData = {};
+  }
   
   function save() {
     fs.writeFileSync(userDataRoot + '/storage.json', JSON.stringify(storageData));
